43093a6deddda3ef9e20d10714d28a55c8fc4514
naza-vo <vickiedaniels500@gmail.com>
2026-08-13 23:53:22 +0100

    learning javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+//Number methods
+// let num = 356;
+// let num2 = 757.984765;
+// let score = "34";
+
+// console.log(num + 34);
+// console.log(num.toString() + 34);
+// console.log(num2.toFixed(4));
+// console.log(parseInt(score) + 10); 
+
+// let a = 27;
+// let b = 5;
+// let c = 15; 
+
+// console.log(a +  b);
+// console.log(a % b);
+
+
+//logical
+
+let uniform=true;
+let ID =  false;
+let textbook = true;
+
+console.log(uniform && ID)
