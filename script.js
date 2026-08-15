5c8a539e05fdd51db2e747b11bc179535c8bbab9
naza-vo <vickiedaniels500@gmail.com>
2026-08-15 21:08:32 +0100

    if condition

--- script.js
+++ script.js
@@ -36,9 +36,102 @@ let vote = age < 18 ? "you can vote" : "you cannot vote";
 console.log(vote);
 
 // prompt and alert
-alert("Hello Everyone");
-alert("Jesus is king");
+// alert("Hello Everyone");
+// alert("Jesus is king");
 
-let newage = prompt("Enter your age");
-let newvote = newage >= 18 ? "you can vote" :"You cannot vote";
-// alert(newvote); 
+// let newage = prompt("Enter your age");
+// let newvote = newage >= 18 ? "you can vote" :"You cannot vote";
+// // alert(newvote); 
+
+
+// let a = parseInt(prompt("Enter your first number"));
+// let b = parseInt(prompt("Enter your second number"))
+// alert(a + b)
+
+
+// conditionals
+if (5 > 7) {console.log("5 is the greatest number");
+} else{
+    console.log("7 is the greatest number");
+}
+
+
+let num = 5;
+if(num > 5){
+    alert("Round to 1");
+} else if (num == 5) {
+    alert("round to 1");
+} else{
+alert("round to zero");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
